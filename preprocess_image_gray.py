87b4306d44137eb89bee3deb499546b752501401
kimwj94 <kimwj317@naver.com>
2022-06-14 12:08:58 +0900

    fix gray

--- preprocess_image_gray.py
+++ preprocess_image_gray.py
@@ -1,13 +1,15 @@
 import os
 from utils_gray import *
+from options import get_args
 if __name__ == "__main__":
-    base_path = "/home/aiot/alpaca/alpaca_crowd/"
-    train_folder = os.path.join(base_path, "train_images") # 원본 이미지 폴더
-    val_folder = os.path.join(base_path, "val_images")
+    args = get_args()
+    base_path = args.base_path
+    train_folder = os.path.join(base_path, args.train_folder) # 원본 이미지 폴더
+    val_folder = os.path.join(base_path, args.val_folder)
     
-    processed_train_folder = os.path.join(base_path, "preprocessed_train_gray") # Resize 된 이미지 폴더
-    processed_valid_folder = os.path.join(base_path, "preprocessed_valid_gray")
-    INPUT_IMAGE_SIZE = (336, 336)
+    processed_train_folder = os.path.join(base_path, args.proc_train_folder) # Resize 된 이미지 폴더
+    processed_valid_folder = os.path.join(base_path, args.proc_val_folder)
+    INPUT_IMAGE_SIZE = (args.target_size, args.target_size)
     
     preprocess_images(train_folder, processed_train_folder, image_size = INPUT_IMAGE_SIZE)
     preprocess_images(val_folder, processed_valid_folder, image_size = INPUT_IMAGE_SIZE)
--- train_gray.py
+++ train_gray.py
@@ -6,12 +6,12 @@ if __name__ == "__main__":
     parser.add_argument("--multi_gpu", default=0, type=int)
     args = parser.parse_args()
 
-    base_path = "/home/aiot/alpaca/alpaca_crowd/"
+    base_path = args.base_path
     processed_train_folder = os.path.join(base_path, "preprocessed_train_gray") # Resize 된 이미지 폴더
     processed_valid_folder = os.path.join(base_path, "preprocessed_valid_gray")
 
-    train_density_maps_pkl = "train_gt_density_maps_84x84_cleaned.pkl" # gt density map 생성 피클 파일명
-    valid_density_maps_pkl = "valid_gt_density_maps_84x84_cleaned.pkl"
+    train_density_maps_pkl = os.path.join(base_path, args.train_pkl_file) # gt density map 생성 피클 파일명
+    valid_density_maps_pkl = os.path.join(base_path, args.valid_pkl_file)
 
     if args.multi_gpu:
         strat = tf.distribute.MirroredStrategy()
